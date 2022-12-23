ab55061ef36f2f5c80a04195850e1c3c8e22ff8a
generali.collins <104697851+generali-collins@users.noreply.github.com>
2022-12-23 13:51:59 +0000

    Add files via upload
    Using the python function module to write a compound interest calculation formula

---
+++ compondInterest formula.py
@@ -0,0 +1,8 @@
+def calculateAmount(principal,rate,time,numTimes):
+    amount = principal*(1+(rate/numTimes))**(numTimes*time)
+    return amount
+
+amount = calculateAmount(1000,0.15,5,12)
+
+print(amount)
+
